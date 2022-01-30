6e7b2c4c3dbd5d52a772f1cad23ea5448cf8b449
ebrahimshehata <engebrahimessamshehata@gmail.com>
2022-01-30 15:53:10 +0200

    after report

--- app/src/main/java/com/app/pethouse/activities/admin/TypesActivity.java
+++ app/src/main/java/com/app/pethouse/activities/admin/TypesActivity.java
@@ -1,89 +1,127 @@
 package com.app.pethouse.activities.admin;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.app.pethouse.R;
-import com.app.pethouse.activities.admin.fragment.TypePageAdapter;
+import com.app.pethouse.adapter.TypeAdapter;
 import com.app.pethouse.callback.TypeCallback;
 import com.app.pethouse.controller.TypeController;
-import com.app.pethouse.dialogs.CityDialog;
 import com.app.pethouse.dialogs.DataDialog;
+import com.app.pethouse.dialogs.RateDialog;
 import com.app.pethouse.model.TypeModel;
+import com.app.pethouse.utils.LoadingHelper;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 
-public class TypesActivity extends AppCompatActivity implements DataDialog.DataDialogListener{
-    View mRootView;
-    int currentTab;
+public class TypesActivity extends AppCompatActivity implements TypeAdapter.TypeListener, DataDialog.DataDialogListener{
+     LoadingHelper loadingHelper;
+     FloatingActionButton add;
+     LinearLayout root;
+     TypeAdapter adapter;
+     RecyclerView recyclerView;
+     TextView noList;
+     ArrayList<TypeModel> currentList = new ArrayList<>();
+     TypeModel chosenType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_types);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        Intent intent = getIntent();
-        currentTab = intent.getIntExtra("tab", 0);
-        mRootView = findViewById(R.id.root);
-        TypePageAdapter dataSectionsPageAdapter = new TypePageAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(dataSectionsPageAdapter);
-        final TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-        final FloatingActionButton fab = findViewById(R.id.fab);
+        add = findViewById(R.id.fab);
+        loadingHelper = new LoadingHelper(this);
+        root = findViewById(R.id.root);
+        recyclerView = findViewById(R.id.recycler_view);
+        noList = findViewById(R.id.no_items);
 
-        tabs.selectTab(tabs.getTabAt(currentTab));
+        // perform click event on button
+        add.setOnClickListener(v -> {
+            RateDialog dialog = new RateDialog(false);
+            dialog.show(getSupportFragmentManager(), "dialog");
+        });
 
-        fab.setOnClickListener(view -> {
-            if(tabs.getSelectedTabPosition() == 0) { //Gov
-                currentTab = 0;
-                DataDialog dialog = new DataDialog();
-                dialog.show(getSupportFragmentManager() , "data dialog");
-            }else if(tabs.getSelectedTabPosition() == 1) { //Cities
-                currentTab = 1;
-                CityDialog dialog = new CityDialog();
-                dialog.show(getSupportFragmentManager() , "data dialog");
+        new TypeController().getTypes(new TypeCallback() {
+            @Override
+            public void onSuccess(ArrayList<TypeModel> rates) {
+                loadingHelper.dismissLoading();
+                currentList = rates;
+                noList.setText("No Types Found!");
+                if (currentList.size() > 0) {
+                    recyclerView.setVisibility(View.VISIBLE);
+                    noList.setVisibility(View.GONE);
+                    if (adapter == null || adapter.getData().size() == 0) {
+                        adapter = new TypeAdapter(currentList, TypesActivity.this);
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(TypesActivity.this));
+                    } else {
+                        adapter.updateData(currentList);
+                    }
+                } else {
+                    noList.setVisibility(View.VISIBLE);
+                    recyclerView.setVisibility(View.GONE);
+                }
+
+            }
+            @Override
+            public void onFail(String msg) {
+                loadingHelper.dismissLoading();
+                Toast.makeText(TypesActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(int position) {
+        chosenType = adapter.getData().get(position);
+        DataDialog dialog = new DataDialog(adapter.getData().get(position).getName());
+        dialog.show(TypesActivity.this.getSupportFragmentManager(), "dialog");
+    }
+
+    @Override
+    public void deleteItem(int position) {
+        loadingHelper.showLoading("");
+        new TypeController().delete(adapter.getData().get(position), new TypeCallback() {
+            @Override
+            public void onSuccess(ArrayList<TypeModel> types) {
+                loadingHelper.dismissLoading();
+                Toast.makeText(TypesActivity.this, "deleted!", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFail(String error) {
+                loadingHelper.dismissLoading();
+                Toast.makeText(TypesActivity.this, error, Toast.LENGTH_LONG).show();
             }
         });
     }
 
     @Override
-    public void onBackPressed(){
-        Intent intent = new Intent(this, AdminMainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-    }
-
-    private void showSnackbar(String message) {
-        Snackbar.make(mRootView, message, Snackbar.LENGTH_LONG).show();
-    }
-    @Override
     public void getData(String name) {
-        if(currentTab == 0){
-            new TypeController().newType(name, new TypeCallback() {
-                @Override
-                public void onSuccess(ArrayList<TypeModel> types) {}
-
-                @Override
-                public void onFail(String error) {
-                    showSnackbar(error);
-                }
-            });
-        }
+        chosenType.setName(name);
+        new TypeController().save(chosenType, new TypeCallback() {
+            @Override
+            public void onSuccess(ArrayList<TypeModel> rates) {
+                Toast.makeText(TypesActivity.this, "Send", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFail(String msg) {
+                Toast.makeText(TypesActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-
 
     @Override
     public void onError(String error) {
-        showSnackbar(error);
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
+
     }
 }
